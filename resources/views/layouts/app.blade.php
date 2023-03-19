d5aa7399afd306b6271d440e1930303d5bf3c2c3
Demilade362 <ademolademilade362@gmail.com>
2023-03-20 00:11:02 +0100

    Updated The readme File

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }} Staff</a>
                                 </li>
                             @endif
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app');
 @section('content')
-    <div class="my-5 text-center">
+    {{-- <div class="my-5 text-center">
         <h1 class="display-1">Pizza House</h1>
         <p>{{ session('mssg') }}</p>
         <a href="/pizzas/create" class="btn btn-primary my-2">
             Order A Pizza
         </a>
+    </div> --}}
+
+    <div class="container bg-white p-2 shadow-sm">
+        <h1 class="display-5">Pizza House</h1>
     </div>
 @endsection
